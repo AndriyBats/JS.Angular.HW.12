19c95bed36b5f4c22b4a8bb4482143db9409b933
Andriy Bats <abats333@gmail.com>
2021-09-18 01:40:29 +0300

    add JS.Angular.HW.12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Homework12Component } from './homework12/homework12.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Homework12Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
